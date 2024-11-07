3684297d8b4cf3460d0b32343bff4c4b405d90ae
vishalBelwal <vishalbelwal52@gmail.com>
2024-11-08 03:01:20 +0530

    homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-// import { useSession } from "@clerk/nextjs";
-// import { MeetingTypeLists } from "@/components/MeetingTypeLists";
+import { CardsList } from "@/components/CardsList";
+import { useSession } from "@clerk/nextjs";
 
 export default function Home() {
   const now = new Date();
@@ -9,25 +9,31 @@ export default function Home() {
     hour: "2-digit",
     minute: "2-digit",
   });
-  const date = new Intl.DateTimeFormat("en-IN", { dateStyle: "full" }).format(now);
+  const date = new Intl.DateTimeFormat("en-IN", { dateStyle: "full" }).format(
+    now
+  );
 
-//   const { session } = useSession();
-//   const user = session?.user;
+  const { session } = useSession();
+  const user = session?.user;
 
   return (
     <div className="flex size-full flex-col gap-10 text-white">
-      <div className="h-[300px] w-full bg-hero bg-cover">
-        <div className="flex flex-col h-full justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-tighter font-semibold text-center text-lg">
-            {/* Hello, {user?.username?.toLocaleUpperCase()} */}
-          </h2>
-
+      <div className="h-[300px] w-full bg-cover">
+        <div className="flex h-full justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
-            <p className="text-lg font-semibold text-sky-1 lg:text-2xl">{date}</p>
+            <h1 className="text-4xl font-extrabold lg:text-7xl text-orange-300">{time}</h1>
+            <p className="text-lg font-semibold text-sky-1 lg:text-2xl">
+              {date}
+            </p>
           </div>
+
+          <h2 className="glassmorphism max-h-[120px] p-4 rounded max-sm:max-h-[120px] text-wrap font-semibold text-center text-lg flex flex-col">
+            Hello,<span className="text-yellow-300">{user?.username?.toLocaleUpperCase()}</span>😊
+          </h2>
         </div>
       </div>
+
+      <CardsList />
     </div>
   );
 }
